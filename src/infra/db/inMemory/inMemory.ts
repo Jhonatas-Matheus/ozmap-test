7d63d1f836d7896cee6c710ddc83f9fa9dced577
Jhonatas <jhonatasoliveira021@gmail.com>
2023-06-05 15:57:59 -0300

    feat: add repository in memory - implementation database in memory of userRepository

---
+++ src/infra/db/inMemory/inMemory.ts
@@ -0,0 +1,56 @@
+import { use } from "chai";
+import { CreateUserDto, ListUserDto, UpdateUserDto } from "../../../domain/user.dto";
+import { User } from "../../../domain/user.entity";
+import { UserRepositoryInterface } from "../../../domain/user.repository";
+
+
+
+export class UserInMemoryRepository implements UserRepositoryInterface {
+    users: User[]
+    constructor(){
+        this.users= []
+    }
+    async save(user: User): Promise<User> {
+        this.users.push(user)
+        return user
+    }
+    async listSpecificUser(userId: string): Promise<User> {
+        const userFound = this.users.find((user)=> user.id === userId)
+        if(userFound){
+            return userFound
+        }
+        throw new Error("User not found");
+    }
+    async listAll(): Promise<User[]> {
+        return this.users
+    }
+    async findAndUpdate(userId: string, updatedUserInfo: UpdateUserDto): Promise<User> {
+        const userToBeUpdated = this.users.find((user)=> user.id === userId)
+        const userToBeUpdatedIndex = this.users.findIndex((user)=> user.id === userId)
+        if(userToBeUpdated){
+            const currentUser = new User(userToBeUpdated)
+            if(updatedUserInfo.age){
+                currentUser.updateAge(updatedUserInfo.age)
+            }
+            if(updatedUserInfo.name){
+                currentUser.updateName(updatedUserInfo.name)
+            }
+            if(updatedUserInfo.email){
+                currentUser.updateEmail(updatedUserInfo.email)
+            }
+            this.users.splice(userToBeUpdatedIndex,0, currentUser)
+            return currentUser
+        }
+        throw new Error("User not found");
+    }
+    async delete(idUser: string): Promise<void> {
+        const userToBeDeleted = this.users.findIndex(user=> user.id === idUser)
+        if(userToBeDeleted){
+           this.users.splice(userToBeDeleted,1)
+           return
+        }
+        throw new Error("User not found");
+    }
+    
+
+}
