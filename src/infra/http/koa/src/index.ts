a6491ee59677b5aa744bc59cd8eba9627a394e0b
Jhonatas <jhonatasoliveira021@gmail.com>
2023-06-06 07:53:23 -0300

    chore: config swagger for implementation with koa

--- src/infra/http/koa/src/index.ts
+++ src/infra/http/koa/src/index.ts
@@ -6,6 +6,9 @@ import { Server } from 'http';
 import cors from '@koa/cors'
 import { AppDataSource } from '../../../db/typeorm/data-source';
 import { router } from './routes';
+import path from 'path';
+import yamljs from 'yamljs';
+import { koaSwagger } from 'koa2-swagger-ui';
 
 export const koa = new Koa()
 
@@ -17,11 +20,16 @@ koa.use(cors())
 koa.use(bodyParser());
 koa.use(router.routes())
 koa.use(router.allowedMethods());
+const filePath = path.join(__dirname, 'openapi.yaml');
+const spec = yamljs.load(filePath);
+router.get('/docs', koaSwagger({ routePrefix: false, swaggerOptions: { spec } }));
 export let server: Server
 const startServer = async ()=>{
     try {
         connectionDatabase()
-        server = koa.listen(3000)
+        server = koa.listen(process.env.PORT? process.env.PORT : 3000,()=>{
+            console.log(`Aplicação rodando na porta ${process.env.PORT? process.env.PORT : 3000}`)
+        })
     } catch (error) {
         console.log(error)
     }
